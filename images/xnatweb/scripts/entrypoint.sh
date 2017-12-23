d6f5ce36287460c71d747d6f181e2517cd527d2a
lomadi <lomadi@mac.com>
2017-12-23 15:17:09 +0100

    change to official postgres container

--- images/xnatweb/scripts/entrypoint.sh
+++ images/xnatweb/scripts/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo "WELCOME IN THE BIBBOX XNAT APP"
+echo "=========== WELCOME IN THE BIBBOX XNAT APP ==========="
 
 #adapt the password to access the postgres DB
 sed -i "s#changethispasswordinproductionenvironments#${DB_PASSWORD}#g" /data/xnat/home/config/xnat-conf.properties
+echo "  postgres password was patched in /data/xnat/home/config/xnat-conf.properties"
+echo "======================================================"
 
 #start the tomact sever
 $CATALINA_HOME/bin/startup.sh "run"
